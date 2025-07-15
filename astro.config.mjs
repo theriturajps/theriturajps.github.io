4ff2cd3b2135e79cc7f45998ce4fc774619681fb
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2025-07-15 12:40:46 +0530

    fix: update branch name from 'main' to 'master' in deploy workflow; change site URL in astro config; add .nojekyll file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://theriturajps.github.io',
+  site: 'https://me.letnote.in',
   base: '/',
   output: 'static'
 });
